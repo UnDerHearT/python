213f090f8412867894f0446957b35b36e95f83cc
UnDerHearT <43998898+UnDerHearT@users.noreply.github.com>
2020-05-02 17:35:11 +0700

    GPA

---
+++ GPA.py
@@ -0,0 +1,18 @@
+while True:
+        score=float(input("Please enter a score.:"))
+        if (score >=80.5 and score <=100):
+                        print ('A')
+        elif (score >=75.5 and score <=79):
+                        print ('B+')
+        elif (score >=70.5 and score <=74.5):
+                        print ('B')
+        elif (score >=65.5 and score <=69.5):
+                        print ('C+')
+        elif (score >=60.5 and score <=64.5):
+                        print ('C')
+        elif (score >=0 and score <=59.5):
+                        print ('F')
+        else:
+                        print('Error, pls try again.')   
+        
+
